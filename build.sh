ffd77a51b09743f5c9886e5718d7a66c1c6f1582
Niek Candaele <niekcandaele@gmail.com>
2019-02-08 23:26:39 +0100

    Add travis configuration

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-mkdir build/
+mkdir tmp/ dist/
 echo "Copying icons for version $1 - Searching for: '$1 - *'"
-cp sdtdIcons/"$1 - "*/* build/
+cp sdtdIcons/"$1 - "*/* tmp/
+tar -czf dist/"sdtdIcons - $1".tar.gz tmp/
+rm -rf tmp/
